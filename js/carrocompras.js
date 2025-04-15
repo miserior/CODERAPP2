b2b7a53d3ae58741cda74f17c9e0abc1da98c1c0
miserior <51886505+miserior@users.noreply.github.com>
2025-04-14 22:33:49 -0500

    Se actualizo con el proyecto de desarrollo web

---
+++ js/carrocompras.js
@@ -0,0 +1,75 @@
+// Declaro variables, objetos funciones
+function eliminarCarrito() {
+  addButton = document.querySelectorAll(".productoQuitar");
+  addButton.forEach((button) => {
+    button.onclick = (e) => {
+      const productoIdAEliminar = e.currentTarget.id;
+      const indiceProducto = carrito.findIndex(
+        (item) => item.idProducto == productoIdAEliminar
+      );
+      if (indiceProducto !== -1) {
+        carrito.splice(indiceProducto, 1);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        renderCarrito(carrito);
+      }
+    };
+  });
+}
+
+function renderCarrito(productosCarrito) {
+  carritoContainer.innerHTML = "";
+  const botonComprar = document.getElementById("boton-comprar");
+  let totalCarritoContainer = document.getElementById(
+    "total-carrito-container"
+  );
+
+  if (!productosCarrito || productosCarrito.length === 0) {
+    if (botonComprar) {
+      botonComprar.style.display = "none";
+      totalCarritoContainer.innerHTML = `<p>El carrito esta vacio</p>`;
+    }
+    return;
+  }
+  productosCarrito.forEach((productoCarrito) => {
+    const card = document.createElement("div");
+    card.classList.add(
+      "card",
+      "sectionc-cards",
+      "col-lg-3",
+      "col-md-2",
+      "col-sm-12"
+    );
+    card.innerHTML = `
+      <div class="card-body">
+      <img src=${productoCarrito.enlace} alt=${productoCarrito.producto}>
+      <h3 class="card-title">${productoCarrito.producto}</h3>
+      <p class="card-text">${productoCarrito.valor
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ".")} COP</p>
+        <p class="card-text">Unidades añadidas: ${productoCarrito.cantidad} </p>
+     <button type="button" class="btn btn-danger productoQuitar" id=${
+       productoCarrito.idProducto
+     }>Eliminar del carrito</button>
+      </div>`;
+    carritoContainer.appendChild(card);
+  });
+  eliminarCarrito();
+
+  const total = productosCarrito.reduce((acumulador, producto) => {
+    return acumulador + producto.valor * producto.cantidad;
+  }, 0);
+
+  if (total > 0) {
+    totalCarritoContainer.innerHTML = `
+    <h4>Total carrito:</h4>
+    <p>$ ${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} COP</p>
+    `;
+    carritoContainer.appendChild(totalCarritoContainer);
+  }
+}
+
+let carrito = localStorage.getItem("carrito");
+carrito = JSON.parse(carrito);
+
+let carritoContainer = document.getElementById("carrito-contenedor");
+renderCarrito(carrito);
--- js/main.js
+++ js/main.js
@@ -1,79 +1,133 @@
-// Declaro funciones que se utilizaran 
-
-
-/** Genera una lista de productos disponibles recibe un array de objetos y retorna un string que formateado */
-function mostrarProductos(productos) {
-  let listaProductos =
-    "Seleccione el 'ID' del producto que desea agregar al carrito: \n\n";
-  for (const producto of productos) {
-    const valorFormateado = producto.valor
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    listaProductos += `${producto.idProducto} - ${producto.producto}: $ ${valorFormateado}\n`;
-  }
-  listaProductos += "o ingrese 'finalizar' para terminar la compra.";
-  return listaProductos;
-}
-
-// Declaro variables u objetos 
+// Declaro variables, objetos funciones
 
 const productos = [
-  { idProducto: 1, producto: "DJI Neo", valor: 1049900, enlace: ''},
-  { idProducto: 2, producto: "DJI Mini 4 Pro", valor: 4449900,enlace: '' },
-  { idProducto: 3, producto: "DJI Avata 2", valor: 5619900, enlace: ''},
-  { idProducto: 4, producto: "DJI Matrice 4", valor: 24999900, enlace: ''},
-  { idProducto: 5, producto: "DJI Air 3", valor: 5869900, enlace: ''},
+  {
+    idProducto: 1,
+    producto: "DJI Neo",
+    valor: 1049900,
+    enlace:
+      "../assets/img/producto-neo.png",
+  },
+  {
+    idProducto: 2,
+    producto: "DJI Mini 4 Pro",
+    valor: 4449900,
+    enlace:
+      "../assets/img/producto-mini-4-pro.jpeg",
+  },
+  {
+    idProducto: 3,
+    producto: "DJI Avata 2",
+    valor: 5619900,
+    enlace:
+      "../assets/img/producto-avata2.png",
+  },
+  {
+    idProducto: 4,
+    producto: "DJI Matrice 4",
+    valor: 24999900,
+    enlace:
+      "../assets/img/producto-matrice-350.png",
+  },
+  {
+    idProducto: 5,
+    producto: "DJI Air 3",
+    valor: 5869900,
+    enlace:
+      "../assets/img/producto-air-3.png",
+  },
 ];
 
-let totalCompra = 0;
-let estadoCompra = "comprando";
-
-// Inicio programación
-
-alert("Bienvenido a la tienda de drones");
-
-// El usuario debera digitar id validos o escribir finalizar para salir del ciclo 
-
-while (estadoCompra !== "finalizar") {
-  const seleccion = prompt(mostrarProductos(productos));
- // se contempla cuando da cancelar en prompt 
-  if (seleccion === "finalizar" || seleccion === null) {
-    estadoCompra = "finalizar";
-  } else {
-    const idSeleccionado = parseInt(seleccion);
-    const productoSeleccionado = productos.find(
-      (producto) => producto.idProducto === idSeleccionado
-    );
-
-    if (productoSeleccionado) {
-      totalCompra += productoSeleccionado.valor;
-      alert(`${productoSeleccionado.producto} añadido al carrito.`);
-    } else {
-      alert(
-        "Producto no válido. Por favor, seleccione un 'ID' de producto válido."
-      );
-    }
-  }
-}
-
-// si la compra es 0 porque finalizo repentinamente
-
-if (totalCompra > 0) {
-  const confirmarCompra = confirm(
-    `¿Desea confirmar la compra por un total de: $ ${totalCompra
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}?`
-  );
-
-  if (confirmarCompra) {
-    alert(
-      `Compra confirmada. Total: $ ${totalCompra
-        .toString()
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`
-    );
-  } else {
-    alert("Compra cancelada.");
-  }
+let carrito = localStorage.getItem("carrito");
+if (carrito) {
+  carrito = JSON.parse(carrito);
 } else {
-  alert("No se agregaron productos al carrito.");
+  carrito = [];
 }
+
+function agregarCarrito() {
+  addButton = document.querySelectorAll(".productoAgregar");
+  addButton.forEach((button) => {
+    button.onclick = (e) => {
+      const productoId = e.currentTarget.id;
+      const productoSeleccionado = productos.find(
+        (producto) => producto.idProducto == productoId
+      );
+      const productoEnCarrito = carrito.find(
+        (item) => item.idProducto == productoId
+      );
+      if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+      } else {
+        productoSeleccionado.cantidad = 1;
+        carrito.push(productoSeleccionado);
+      }
+      console.log(carrito);
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      productsContainer.innerHTML = '';
+      renderProductos(productos);
+    };
+  });
+}
+
+function eliminarCarrito() {
+  addButton = document.querySelectorAll(".productoQuitar");
+  addButton.forEach((button) => {
+    button.onclick = (e) => {
+      const productoIdAEliminar = e.currentTarget.id;
+      const indiceProducto = carrito.findIndex(
+        (item) => item.idProducto == productoIdAEliminar
+      );
+      if (indiceProducto !== -1) {
+        carrito[indiceProducto].cantidad--
+        if (carrito[indiceProducto].cantidad == 0) 
+        {
+          carrito.splice(indiceProducto, 1);
+        }
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        productsContainer.innerHTML = ''
+        renderProductos(productos)
+      }
+    };
+  });
+}
+
+function renderProductos(productos) {
+  productos.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add(
+      "card",
+      "sectionc-cards",
+      "col-lg-3",
+      "col-md-2",
+      "col-sm-12"
+    );
+    const productoEnCarrito = carrito.find(item => item.idProducto === producto.idProducto)
+    
+    const cantidadEnCarrito = productoEnCarrito ? (productoEnCarrito.cantidad !== undefined ? productoEnCarrito.cantidad : '') : 0
+
+    card.innerHTML = `
+    <div class="card-body">
+    <img src=${producto.enlace} alt=${producto.producto}>
+    <h3 class="card-title">${producto.producto}</h3>
+    <p class="card-text">${producto.valor
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")} COP</p>
+     <p class="card-text">Unidades añadidas: ${cantidadEnCarrito} </p>
+    <button type="button" class="btn btn-dark productoAgregar" id=${
+      producto.idProducto
+    }>Añadir al carrito</button>
+    <button type="button" class="btn btn-danger productoQuitar" id=${
+      producto.idProducto
+    }>Quitar del carrito</button>
+    </div>`;
+    productsContainer.appendChild(card);
+  });
+  agregarCarrito();
+  eliminarCarrito();
+}
+
+// Declaro variables, objetos funciones
+
+let productsContainer = document.getElementById("productos-contenedor");
+renderProductos(productos);
